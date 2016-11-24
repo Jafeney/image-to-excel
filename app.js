779bc7cd73258102f2ed00974353f08ecebb2894
Jafeney <692270687@qq.com>
2016-11-24 13:39:48 +0800

    更新
    更新

--- app.js
+++ app.js
@@ -1,60 +1,24 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+/**
+ * @desc 把图片名称导入到excel
+ * @author Jafeney <692270687@qq.com>
+ * @dateTime 2016-11-24
+ **/
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var fs = require('fs');
+var json2xls = require('json2xls');
+var SOURCE_PATH = './source/';
+var OUTPUT_PATH = './output/';
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+var app = function() {
+    var files = fs.readdirSync(SOURCE_PATH);
+    var data = []
+    files.forEach(function(item, i) {
+        data.push({
+            url: item
+        })
+    })
+    var xls = json2xls(data);
+    fs.writeFileSync(OUTPUT_PATH+'data.xlsx', xls, 'binary');
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+app();
